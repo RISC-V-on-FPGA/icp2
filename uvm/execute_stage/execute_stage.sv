45c6e558dd0e7e4917abbd64a01680c1e1e273ea
NilsPalm1 <np200103@gmail.com>
2024-11-11 11:40:13 +0100

    gfdbdfvasgdh

---
+++ uvm/execute_stage/execute_stage.sv
@@ -0,0 +1,95 @@
+// `include "common.sv"
+// `include "alu.sv"
+// `include "forwarding_unit.sv"
+
+import common::*;
+
+module execute_stage (
+    input                     clk,
+    input              [31:0] pc,
+    //input              rst, Might not be needed due to execute stage being comb
+    input control_type        control_in,
+    input logic        [31:0] data1,
+    input logic        [31:0] data2,
+    input logic        [31:0] immediate_data,
+    input              [ 4:0] rd_in,
+    input              [ 4:0] rs1,
+    input              [ 4:0] rs2,
+    input              [ 4:0] ex_mem_rd,
+    input              [ 4:0] mem_wb_rd,
+    input                     ex_mem_RegWrite,
+    input                     mem_wb_RegWrite,
+    input              [31:0] forward_ex_mem,   // Value forwarded from mem stage, better name?
+    input              [31:0] forward_mem_wb,   // Value from write back stage
+
+    output control_type control_out,
+    output logic ZeroFlag,
+    output logic [31:0] alu_data,
+    output logic [31:0] memory_data,
+    output logic [4:0] rd_out,
+    output logic [31:0] pc_out
+);
+
+  logic [31:0] right_operand;
+  logic [31:0] left_operand;
+
+  mux_control_type mux_ctrl_left;
+  mux_control_type mux_ctrl_right;
+
+  alu alu (
+      .control(control_in.ALUOp),
+      .left_operand(left_operand),
+      .right_operand(right_operand),
+      .ZeroFlag(ZeroFlag),
+      .result(alu_data)
+  );
+
+  forwarding_unit forwarding_unit (
+      .rs1(rs1),
+      .rs2(rs2),
+      .rd(rd_in),
+      .ex_mem_rd(ex_mem_rd),
+      .mem_wb_rd(mem_wb_rd),
+      .mem_wb_RegWrite(mem_wb_RegWrite),
+      .ex_mem_RegWrite(ex_mem_RegWrite),
+      .mux_ctrl_left(mux_ctrl_left),
+      .mux_ctrl_right(mux_ctrl_right)
+  );
+
+  always_comb begin : operand_selector
+    // Deafult
+    left_operand  = data1;
+    right_operand = data2;
+    memory_data   = data2;
+
+    // Forwarding left operand (A)
+    case (mux_ctrl_left)
+      Forward_ex_mem: left_operand = forward_ex_mem;
+      Forward_mem_wb: left_operand = forward_mem_wb;
+      default: ;
+    endcase
+
+    // Forwarding right operand (B)
+    case (mux_ctrl_right)
+      Forward_ex_mem: begin
+        right_operand = forward_ex_mem;
+        memory_data   = forward_ex_mem;
+      end
+      Forward_mem_wb: begin
+        right_operand = forward_mem_wb;
+        memory_data   = forward_mem_wb;
+      end
+      default: ;
+    endcase
+
+    // Loading immidiate value
+    if (control_in.ALUSrc) begin
+      right_operand = immediate_data;
+    end
+  end
+
+  assign control_out = control_in;
+  assign rd_out = rd_in;
+  assign pc_out = pc;
+
+endmodule
---
+++ uvm/execute_stage/forwarding_unit.sv
@@ -0,0 +1,61 @@
+// `include "common.sv"
+import common::*;
+
+module forwarding_unit (
+    input [4:0] rs1,
+    input [4:0] rs2,
+    input [4:0] rd,
+    input [4:0] ex_mem_rd,
+    input [4:0] mem_wb_rd,
+    input mem_wb_RegWrite,
+    input ex_mem_RegWrite,
+    output mux_control_type mux_ctrl_left,
+    output mux_control_type mux_ctrl_right
+);
+
+  always_comb begin : Combinational
+    // if (MEM/WB.RegWrite
+    // and (MEM/WB.RegisterRd ≠ 0)
+    // and not(EX/MEM.RegWrite and (EX/MEM.RegisterRd ≠ 0)
+    //      and (EX/MEM.RegisterRd = ID/EX.RegisterRs1))
+    // and (MEM/WB.RegisterRd = ID/EX.RegisterRs1)) ForwardA = 01
+
+    // if (MEM/WB.RegWrite
+    // and (MEM/WB.RegisterRd ≠ 0)
+    // and not(EX/MEM.RegWrite and (EX/MEM.RegisterRd ≠ 0)
+    //      and (EX/MEM.RegisterRd = ID/EX.RegisterRs2))
+    // and (MEM/WB.RegisterRd = ID/EX.RegisterRs2)) ForwardB = 01
+
+    // if (EX/MEM.RegWrite
+    // and (EX/MEM.RegisterRd ≠ 0)
+    // and (EX/MEM.RegisterRd = ID/EX.RegisterRs1)) ForwardA = 10
+
+    // if (EX/MEM.RegWrite
+    // and (EX/MEM.RegisterRd ≠ 0)
+    // and (EX/MEM.RegisterRd = ID/EX.RegisterRs2)) ForwardB = 10
+
+    if (mem_wb_RegWrite
+        && (mem_wb_rd != 0)
+        && !(ex_mem_RegWrite && (ex_mem_rd != 0) && ex_mem_rd == rs1)
+        && (mem_wb_rd == rs1)) begin
+      mux_ctrl_left = Forward_mem_wb;
+    end else if (ex_mem_RegWrite && (ex_mem_rd != 0) && (ex_mem_rd == rs1)) begin
+      mux_ctrl_left = Forward_ex_mem;
+    end else begin
+      mux_ctrl_left = Forward_def;
+    end
+
+    if (mem_wb_RegWrite
+        && (mem_wb_rd != 0)
+        && !(ex_mem_RegWrite && (ex_mem_rd != 0) && ex_mem_rd == rs2)
+        && (mem_wb_rd == rs2)) begin
+      mux_ctrl_right = Forward_mem_wb;
+    end else if (ex_mem_RegWrite && (ex_mem_rd != 0) && (ex_mem_rd == rs2)) begin
+      mux_ctrl_right = Forward_ex_mem;
+    end else begin
+      mux_ctrl_right = Forward_def;
+    end
+
+  end
+
+endmodule
