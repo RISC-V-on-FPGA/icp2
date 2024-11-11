559e2bb37095a349e470da26944fb9b495326ff6
NilsPalm1 <np200103@gmail.com>
2024-11-11 13:38:37 +0100

    lkhlkavakhd

---
+++ uvm/uvc/pc_uvc/pc_if.sv
@@ -0,0 +1,11 @@
+//------------------------------------------------------------------------------
+// clock_if interface
+//
+// This interface provides a clock output signal.
+// 
+//------------------------------------------------------------------------------
+interface pc_if ();
+    // pc output signal.
+    logic[31:0] pc;
+endinterface : pc_if
+
