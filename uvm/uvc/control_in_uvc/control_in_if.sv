6edaf5861d837db9184d7600b66d63dbff8fe494
NilsPalm1 <np200103@gmail.com>
2024-11-12 15:08:57 +0100

    lsdökjfö

--- uvm/uvc/control_in_uvc/control_in_if.sv
+++ uvm/uvc/control_in_uvc/control_in_if.sv
@@ -4,8 +4,10 @@
 // This interface provides a control_in output signal.
 // 
 //------------------------------------------------------------------------------
-interface control_in_if ();
-    // control_in output signal.                    kanske behöver importera komponenter från kommon för att han skall fatta vad control_type är?????????????????????
-    control_type control_in;
+interface control_in_if (input logic clk, input logic rst_n);
+    // control_in output signal.        kanske behöver importera komponenter från kommon för att han skall fatta vad control_type är?????????????????????
+    
+    // skall denna vara "bit[31:0]" istället -> det är i alla fall vad vi skickar till vår monitor eller är det viktigt med control type när vi skickar saker till DUT ??????
+    control_type control_in;    
 endinterface : control_in_if
 
