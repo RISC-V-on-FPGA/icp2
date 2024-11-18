f912397c860940c7b6b2534ba995d29c21187294
NilsPalm1 <np200103@gmail.com>
2024-11-18 10:27:44 +0100

    kdhflksjdvxzlkn

--- uvm/execute_stage/tb/scoreboard.svh
+++ uvm/execute_stage/tb/scoreboard.svh
@@ -38,6 +38,7 @@ class scoreboard extends uvm_component;
     uvm_analysis_imp_scoreboard_ex_mem_RegWrite #(ex_mem_RegWrite_seq_item, scoreboard) m_ex_mem_RegWrite_ap;
     uvm_analysis_imp_scoreboard_mem_wb_RegWrite #(mem_wb_RegWrite_seq_item, scoreboard) m_mem_wb_RegWrite_ap;
     uvm_analysis_imp_scoreboard_forward_ex_mem #(forward_ex_mem_seq_item, scoreboard) m_forward_ex_mem_ap;
+    uvm_analysis_imp_scoreboard_forward_mem_wb #(forward_mem_wb_seq_item, scoreboard) m_forward_mem_wb_ap;
     uvm_analysis_imp_scoreboard_control_out #(control_out_seq_item, scoreboard) m_control_out_ap;
     uvm_analysis_imp_scoreboard_ZeroFlag #(ZeroFlag_seq_item, scoreboard) m_ZeroFlag_ap;
     uvm_analysis_imp_scoreboard_alu_data #(alu_data_seq_item, scoreboard) m_alu_data_ap;
@@ -103,6 +104,7 @@ class scoreboard extends uvm_component;
         m_ex_mem_RegWrite_ap = new("m_ex_mem_RegWrite_ap", this);
         m_mem_wb_RegWrite_ap = new("m_mem_wb_RegWrite_ap", this);
         m_forward_ex_mem_ap = new("m_forward_ex_mem_ap", this);
+        m_forward_mem_wb_ap = new("m_forward_mem_wb_ap", this);
         m_control_out_ap = new("m_control_out_ap", this);
         m_ZeroFlag_ap = new("m_ZeroFlag_ap", this);
         m_alu_data_ap = new("m_alu_data_ap", this);
@@ -128,6 +130,121 @@ class scoreboard extends uvm_component;
         execute_stage_covergrp.sample();
     endfunction :  write_pc
     
+    virtual function void write_control_in(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("control_in_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        control_in = item.control_in;
+        execute_stage_covergrp.sample();
+    endfunction :  write_control_in
+
+    virtual function void write_data1(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("data1_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        data1 = item.data1;
+        execute_stage_covergrp.sample();
+    endfunction :  write_data1
+
+    virtual function void write_data2(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("data2_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        data2 = item.data2;
+        execute_stage_covergrp.sample();
+    endfunction :  write_data2
+
+    virtual function void write_immediate_data(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("immediate_data_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        immediate_data = item.immediate_data;
+        execute_stage_covergrp.sample();
+    endfunction :  write_immediate_data
+
+    virtual function void write_rd_in(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("rd_in_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        rd_in = item.rd_in;
+        execute_stage_covergrp.sample();
+    endfunction :  write_rd_in
+
+    virtual function void write_rs1(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("rs1_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        rs1 = item.rs1;
+        execute_stage_covergrp.sample();
+    endfunction :  write_rs1
+
+    virtual function void write_rs2(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("rs2_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        rs2 = item.rs2;
+        execute_stage_covergrp.sample();
+    endfunction :  write_rs2
+
+    virtual function void write_ex_mem_rd(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("ex_mem_rd_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        ex_mem_rd = item.ex_mem_rd;
+        execute_stage_covergrp.sample();
+    endfunction :  write_ex_mem_rd
+
+    virtual function void write_mem_wb_rd(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("mem_wb_rd_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        mem_wb_rd = item.mem_wb_rd;
+        execute_stage_covergrp.sample();
+    endfunction :  write_mem_wb_rd
+
+    virtual function void write_ex_mem_RegWrite(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("ex_mem_RegWrite_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        ex_mem_RegWrite = item.ex_mem_RegWrite;
+        execute_stage_covergrp.sample();
+    endfunction :  write_ex_mem_RegWrite
+
+    virtual function void write_mem_wb_RegWrite(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("mem_wb_RegWrite_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        mem_wb_RegWrite = item.mem_wb_RegWrite;
+        execute_stage_covergrp.sample();
+    endfunction :  write_mem_wb_RegWrite
+
+    virtual function void write_forward_ex_mem(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("forward_ex_mem_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        forward_ex_mem = item.forward_ex_mem;
+        execute_stage_covergrp.sample();
+    endfunction :  write_forward_ex_mem
+
+    virtual function void write_forward_mem_wb(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("forward_mem_wb_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        forward_mem_wb = item.forward_mem_wb;
+        execute_stage_covergrp.sample();
+    endfunction :  write_forward_mem_wb
+
+    virtual function void write_control_out(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("control_out_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        control_out = item.control_out;
+        execute_stage_covergrp.sample();
+    endfunction :  write_control_out
+
+    virtual function void write_ZeroFlag(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("ZeroFlag_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        ZeroFlag = item.ZeroFlag;
+        execute_stage_covergrp.sample();
+    endfunction :  write_ZeroFlag
+
+    virtual function void write_alu_data(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("alu_data_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        alu_data = item.alu_data;
+        execute_stage_covergrp.sample();
+    endfunction :  write_alu_data
+
+    virtual function void write_memory_data(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("memory_data_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        memory_data = item.memory_data;
+        execute_stage_covergrp.sample();
+    endfunction :  write_memory_data
+
+    virtual function void write_rd_out(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("rd_out_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        rd_out = item.rd_out;
+        execute_stage_covergrp.sample();
+    endfunction :  write_rd_out
+
+    virtual function void write_pc_out(clk_seq_item item);
+        `uvm_info(get_name(),$sformatf("pc_out_MONITOR:\n%s",item.sprint()),UVM_HIGH)
+        pc_out = item.pc_out;
+        execute_stage_covergrp.sample();
+    endfunction :  write_pc_out
+
+
     //------------------------------------------------------------------------------
     // Check data if both input serial data and output data are valid.
     //------------------------------------------------------------------------------
